a04a26156cbf920286c569f50528a2f2a23db12b
im4kshay <akshaysayre7@gmail.com>
2022-05-12 04:43:49 +0530

    Method to add data into database

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -6,13 +6,49 @@ using System.Threading.Tasks;
 
 namespace ADO.NET
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome");
+
             EmployeeRepo repo = new EmployeeRepo();
-            repo.GetAllEmployees();
-            Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Choose option or press 0 for exit\n1:Retrieve Data\n2:Add Data\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        repo.GetAllEmployees();
+                        break;
+                    case 2:
+                        EmployeeModel model = new EmployeeModel
+                        {
+                            Name = "Saurabh",
+                            Startdate = DateTime.Now,
+                            Gender = 'M',
+                            Phone = 9876453215,
+                            Department = "Testing",
+                            Address = "Katni",
+                            Basic_Pay = 30000.00,
+                            Deductions = 1000.00,
+                            Taxable_Pay = 29000.00,
+                            Income_Tax = 1000.00,
+                            Net_Pay = 28000,
+                        };
+                        repo.AddEmployee(model);
+                        break;
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
